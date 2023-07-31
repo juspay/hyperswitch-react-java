e0a47baf6b7c7b08aab3256522d1f85245480716
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2023-07-31 14:54:10 +0530

    Add comment for business_country and business_label

--- src/main/java/server.java
+++ src/main/java/server.java
@@ -26,6 +26,13 @@ public class server {
 
       response.type("application/json");
 
+      /*
+        If you have two or more “business_country” + “business_label” pairs configured in your Hyperswitch dashboard,
+        please pass the fields business_country and business_label in this request body.
+        For accessing more features, you can check out the request body schema for payments-create API here :
+        https://api-reference.hyperswitch.io/docs/hyperswitch-api-reference/60bae82472db8-payments-create
+      */
+
       String payload = "{ \"amount\": 100, \"currency\": \"USD\" }";
 
       String response_string = createPayment(payload);
